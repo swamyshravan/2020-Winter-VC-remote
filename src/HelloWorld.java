1d4e7663a56c2673dde7b9b42a56a0f31266454e
shravankumar <haha@hehe.com>
2020-10-06 12:37:18 +0200

    Some more input

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,10 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("This is some more output");
         System.out.println("yeah, we are going to github");
+
+        int a = 12;
+        int b = 21;
+        int c = a +b;
+
     }
 }
