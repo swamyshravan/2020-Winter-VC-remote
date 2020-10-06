0150bcf1f6989c2b0c9e7b94dfb143534c407d98
shravankumar <haha@hehe.com>
2020-10-06 12:33:13 +0200

    Some more print

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main ( String[] args ) {
         System.out.println("Hello World");
+        System.out.println("This is some more output");
+        System.out.println("yeah, we are going to github");
     }
 }
